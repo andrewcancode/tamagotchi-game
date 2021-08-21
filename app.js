1720732a63b03cf4c316ae52f918609a8fc1b83e
andrewcancode <alee43207@gmail.com>
2021-08-21 16:50:30 -0400

    basic layout setup

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 console.log('JS Connected!');
+
+/*----- constants -----*/
+/*----- app's state (variables) -----*/
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
+
+const $startGameBtn = $('.startGameBtn');
+const $closeBtn = $('.closeButton');
+const $containerDiv = $('.container');
+
+const showContainer = () => {
+    $containerDiv.addClass('active');
+}
+
+const closeContainer = () => {
+    $containerDiv.removeClass('active');
+}
+
+$startGameBtn.on('click', (e) => {
+    e.preventDefault();
+    showContainer();
+});
+
+$closeBtn.on('click', (e) => {
+    e.preventDefault();
+    closeContainer();
+})
